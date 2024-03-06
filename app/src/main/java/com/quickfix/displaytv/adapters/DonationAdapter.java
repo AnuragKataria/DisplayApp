22625cf622194ec0dfecb7796a5c46cd8d889376
Anurag Kataria <anurag@technavious.com>
2024-03-06 23:32:09 +0530

    Scoll issue fixed

---
+++ app/src/main/java/com/quickfix/displaytv/adapters/DonationAdapter.java
@@ -0,0 +1,120 @@
+package com.quickfix.displaytv.adapters;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.quickfix.displaytv.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class DonationAdapter extends RecyclerView.Adapter<DonationAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<Map<String, String>> donaorsList;
+
+
+    public DonationAdapter(Context context, ArrayList<Map<String, String>> pArrayList) {
+        this.context = context;
+        this.donaorsList = pArrayList;
+
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.layout_donation, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        try {
+            if (position % 2 == 0) {
+                holder.linearLayout.setBackgroundColor(Color.parseColor("#BDBDBD"));
+            } else {
+                holder.linearLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            }
+            holder.txt_sno.setText((position + 1) + ".");
+            holder.txt_fname.setText(donaorsList.get(position).get("firstName") + " " + donaorsList.get(position).get("lastName"));
+            holder.txt_mobile_number.setText(donaorsList.get(position).get("mobile"));
+            holder.txt_jan.setText(donaorsList.get(position).get("jan"));
+            holder.txt_feb.setText(donaorsList.get(position).get("feb"));
+            holder.txt_mar.setText(donaorsList.get(position).get("mar"));
+            holder.txt_apr.setText(donaorsList.get(position).get("apr"));
+            holder.txt_may.setText(donaorsList.get(position).get("may"));
+            holder.txt_jun.setText(donaorsList.get(position).get("jun"));
+            holder.txt_jul.setText(donaorsList.get(position).get("jul"));
+            holder.txt_aug.setText(donaorsList.get(position).get("aug"));
+            holder.txt_sept.setText(donaorsList.get(position).get("sept"));
+            holder.txt_oct.setText(donaorsList.get(position).get("oct"));
+            holder.txt_nov.setText(donaorsList.get(position).get("nov"));
+            holder.txt_dec.setText(donaorsList.get(position).get("dec"));
+            holder.txt_note.setText(donaorsList.get(position).get("note"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return donaorsList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_sno;
+        TextView txt_fname;
+        TextView txt_mobile_number;
+        TextView txt_note;
+        TextView txt_jan;
+        TextView txt_feb;
+        TextView txt_mar;
+        TextView txt_apr;
+        TextView txt_may;
+        TextView txt_jun;
+        TextView txt_jul;
+        TextView txt_aug;
+        TextView txt_sept;
+        TextView txt_oct;
+        TextView txt_nov;
+        TextView txt_dec;
+        LinearLayout linearLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txt_sno = itemView.findViewById(R.id.txt_sno);
+            txt_fname = itemView.findViewById(R.id.txt_fname);
+            txt_mobile_number = itemView.findViewById(R.id.txt_mobile_number);
+            txt_note = itemView.findViewById(R.id.txt_note);
+            txt_jan = itemView.findViewById(R.id.txt_jan);
+            txt_feb = itemView.findViewById(R.id.txt_feb);
+            txt_mar = itemView.findViewById(R.id.txt_mar);
+            txt_apr = itemView.findViewById(R.id.txt_apr);
+            txt_may = itemView.findViewById(R.id.txt_may);
+            txt_jun = itemView.findViewById(R.id.txt_jun);
+            txt_jul = itemView.findViewById(R.id.txt_jul);
+            txt_aug = itemView.findViewById(R.id.txt_aug);
+            txt_sept = itemView.findViewById(R.id.txt_sept);
+            txt_oct = itemView.findViewById(R.id.txt_oct);
+            txt_nov = itemView.findViewById(R.id.txt_nov);
+            txt_dec = itemView.findViewById(R.id.txt_dec);
+            linearLayout = itemView.findViewById(R.id.linearLayout);
+
+        }
+    }
+
+}
--- app/src/main/java/com/quickfix/displaytv/viewpagertransformation/Fragments/TemplateDonationFragment.java
+++ app/src/main/java/com/quickfix/displaytv/viewpagertransformation/Fragments/TemplateDonationFragment.java
@@ -3,6 +3,8 @@ package com.quickfix.displaytv.viewpagertransformation.Fragments;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +16,11 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.quickfix.displaytv.R;
+import com.quickfix.displaytv.adapters.DonationAdapter;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -25,6 +30,7 @@ import java.util.Map;
 public class TemplateDonationFragment extends Fragment {
     private Map<String, Object> productMap;
     private LinearLayout linearTop;
+    private RecyclerView recycleViewDonation;
 
     public TemplateDonationFragment(Map<String, Object> productMap) {
         this.productMap = productMap;
@@ -46,7 +52,39 @@ public class TemplateDonationFragment extends Fragment {
             throw new RuntimeException(e);
         }
     }
+    DonationAdapter donationAdapter;
+    final int speedScroll = 3000;
+    final Handler handler = new Handler();
 
+    final Runnable runnable = new Runnable() {
+        int count = 0;
+        boolean flag = true;
+        @Override
+        public void run() {
+            if(count < donationAdapter.getItemCount()){
+                if(count==donationAdapter.getItemCount()-1){
+                    flag = false;
+                }else if(count == 0){
+                    flag = true;
+                }
+                if(flag) count++;
+                else count--;
+
+                recycleViewDonation.smoothScrollToPosition(count);
+                handler.postDelayed(this,speedScroll);
+            }
+        }
+    };
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        try {
+            handler.removeCallbacks(runnable);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private void assignIds(View v) throws Exception {
         linearTop = v.findViewById(R.id.linearTop);
         LayoutInflater vi = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -54,49 +92,57 @@ public class TemplateDonationFragment extends Fragment {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         ArrayList<Map<String, String>> donaorsList = (ArrayList<Map<String, String>>) donaorsMap.get(year+"");
-        for (int i = 0; i < donaorsList.size(); i++) {
-            View inflaterView = vi.inflate(R.layout.layout_donation, null);
-            LinearLayout linearLayout = inflaterView.findViewById(R.id.linearLayout);
-            if (i % 2 == 0) {
-                linearLayout.setBackgroundColor(Color.parseColor("#BDBDBD"));
-            }else{
-                linearLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        recycleViewDonation = v.findViewById(R.id.recycleViewDonation);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recycleViewDonation.setLayoutManager(linearLayoutManager);
+         donationAdapter = new DonationAdapter(getActivity(),donaorsList );
+        recycleViewDonation.setAdapter(donationAdapter);
+        handler.postDelayed(runnable,speedScroll);
 
-            }
-            TextView txt_sno = inflaterView.findViewById(R.id.txt_sno);
-            TextView txt_fname = inflaterView.findViewById(R.id.txt_fname);
-            TextView txt_mobile_number = inflaterView.findViewById(R.id.txt_mobile_number);
-            TextView txt_note = inflaterView.findViewById(R.id.txt_note);
-            TextView txt_jan = inflaterView.findViewById(R.id.txt_jan);
-            TextView txt_feb = inflaterView.findViewById(R.id.txt_feb);
-            TextView txt_mar = inflaterView.findViewById(R.id.txt_mar);
-            TextView txt_apr = inflaterView.findViewById(R.id.txt_apr);
-            TextView txt_may = inflaterView.findViewById(R.id.txt_may);
-            TextView txt_jun = inflaterView.findViewById(R.id.txt_jun);
-            TextView txt_jul = inflaterView.findViewById(R.id.txt_jul);
-            TextView txt_aug = inflaterView.findViewById(R.id.txt_aug);
-            TextView txt_sept = inflaterView.findViewById(R.id.txt_sept);
-            TextView txt_oct = inflaterView.findViewById(R.id.txt_oct);
-            TextView txt_nov = inflaterView.findViewById(R.id.txt_nov);
-            TextView txt_dec = inflaterView.findViewById(R.id.txt_dec);
-            txt_sno.setText((i + 1) + ".");
-            txt_fname.setText(donaorsList.get(i).get("firstName") + " " + donaorsList.get(i).get("lastName"));
-            txt_mobile_number.setText(donaorsList.get(i).get("mobile"));
-            txt_jan.setText(donaorsList.get(i).get("jan"));
-            txt_feb.setText(donaorsList.get(i).get("feb"));
-            txt_mar.setText(donaorsList.get(i).get("mar"));
-            txt_apr.setText(donaorsList.get(i).get("apr"));
-            txt_may.setText(donaorsList.get(i).get("may"));
-            txt_jun.setText(donaorsList.get(i).get("jun"));
-            txt_jul.setText(donaorsList.get(i).get("jul"));
-            txt_aug.setText(donaorsList.get(i).get("aug"));
-            txt_sept.setText(donaorsList.get(i).get("sept"));
-            txt_oct.setText(donaorsList.get(i).get("oct"));
-            txt_nov.setText(donaorsList.get(i).get("nov"));
-            txt_dec.setText(donaorsList.get(i).get("dec"));
-            txt_note.setText(donaorsList.get(i).get("note"));
-            linearTop.addView(inflaterView);
-        }
+//        for (int i = 0; i < donaorsList.size(); i++) {
+//            View inflaterView = vi.inflate(R.layout.layout_donation, null);
+//            LinearLayout linearLayout = inflaterView.findViewById(R.id.linearLayout);
+//            if (i % 2 == 0) {
+//                linearLayout.setBackgroundColor(Color.parseColor("#BDBDBD"));
+//            }else{
+//                linearLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+//
+//            }
+//            TextView txt_sno = inflaterView.findViewById(R.id.txt_sno);
+//            TextView txt_fname = inflaterView.findViewById(R.id.txt_fname);
+//            TextView txt_mobile_number = inflaterView.findViewById(R.id.txt_mobile_number);
+//            TextView txt_note = inflaterView.findViewById(R.id.txt_note);
+//            TextView txt_jan = inflaterView.findViewById(R.id.txt_jan);
+//            TextView txt_feb = inflaterView.findViewById(R.id.txt_feb);
+//            TextView txt_mar = inflaterView.findViewById(R.id.txt_mar);
+//            TextView txt_apr = inflaterView.findViewById(R.id.txt_apr);
+//            TextView txt_may = inflaterView.findViewById(R.id.txt_may);
+//            TextView txt_jun = inflaterView.findViewById(R.id.txt_jun);
+//            TextView txt_jul = inflaterView.findViewById(R.id.txt_jul);
+//            TextView txt_aug = inflaterView.findViewById(R.id.txt_aug);
+//            TextView txt_sept = inflaterView.findViewById(R.id.txt_sept);
+//            TextView txt_oct = inflaterView.findViewById(R.id.txt_oct);
+//            TextView txt_nov = inflaterView.findViewById(R.id.txt_nov);
+//            TextView txt_dec = inflaterView.findViewById(R.id.txt_dec);
+//            txt_sno.setText((i + 1) + ".");
+//            txt_fname.setText(donaorsList.get(i).get("firstName") + " " + donaorsList.get(i).get("lastName"));
+//            txt_mobile_number.setText(donaorsList.get(i).get("mobile"));
+//            txt_jan.setText(donaorsList.get(i).get("jan"));
+//            txt_feb.setText(donaorsList.get(i).get("feb"));
+//            txt_mar.setText(donaorsList.get(i).get("mar"));
+//            txt_apr.setText(donaorsList.get(i).get("apr"));
+//            txt_may.setText(donaorsList.get(i).get("may"));
+//            txt_jun.setText(donaorsList.get(i).get("jun"));
+//            txt_jul.setText(donaorsList.get(i).get("jul"));
+//            txt_aug.setText(donaorsList.get(i).get("aug"));
+//            txt_sept.setText(donaorsList.get(i).get("sept"));
+//            txt_oct.setText(donaorsList.get(i).get("oct"));
+//            txt_nov.setText(donaorsList.get(i).get("nov"));
+//            txt_dec.setText(donaorsList.get(i).get("dec"));
+//            txt_note.setText(donaorsList.get(i).get("note"));
+//            linearTop.addView(inflaterView);
+//        }
+//
     }
 
 }
