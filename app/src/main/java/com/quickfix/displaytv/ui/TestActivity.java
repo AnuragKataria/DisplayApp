c50c67a32b166e10a10afe3d421f9ade0e54e87d
AnuragKataria <katanu09@gmail,com>
2025-02-13 11:59:01 +0530

    New changes

--- app/src/main/java/com/quickfix/displaytv/ui/TestActivity.java
+++ app/src/main/java/com/quickfix/displaytv/ui/TestActivity.java
@@ -1,6 +1,7 @@
 package com.quickfix.displaytv.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,9 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_template_1);
+        setContentView(R.layout.fragment_template_donation);
+        TextView marqueeText = findViewById(R.id.marqueeText);
+        marqueeText.setText("This is a marquee text in Android. It will scroll continuously!This is a marquee text in Android. It will scroll continuously! This is a marquee text in Android. It will scroll continuously!");
+        marqueeText.setSelected(true);
     }
 }
